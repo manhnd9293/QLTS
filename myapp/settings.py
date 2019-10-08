19236278bf2059853a5c0f8ce84bf00242ae64e6
Nguyen Manh <manhnd9293@gmail.com>
2019-10-08 18:37:15 +0700

    change db engine

--- myapp/settings.py
+++ myapp/settings.py
@@ -76,17 +76,20 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'qlts',
+        'USER': 'qlts',
+        'PASSWORD': '123123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
+    { 
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
