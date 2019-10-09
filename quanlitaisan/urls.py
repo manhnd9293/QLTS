16e9b13b10322c47bcf9d209c15229e73a3c3600
Nguyen Manh <manhnd9293@gmail.com>
2019-10-09 17:04:41 +0700

    update urls config

--- quanlitaisan/urls.py
+++ quanlitaisan/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
   path('', views.index, name = 'index'),
   path('details/<int:asset_id>', views.details, name = 'details'),
   path('maintain/<int:maintain_id>', views.maintains, name = 'maintains'),
-  path('search/', views.search_view, name = 'search'),
+  path('search/', views.SearchView.as_view(), name = 'search'),
   path('addItem/', views.add_item, name = 'add_item'),
   path('addMaintain/', views.add_maintain, name = 'add_maintain'),
   path('assets/<int:page>', views.assets_list, name = 'assets_list'),
-  path('about/',views.MyView.as_view())
+  path('about/', views.MyView.as_view(), name = 'test')
 ]
